6fe34e6a61ace0312ab8dfa7dc1813dab795cb67
Cleyson Lima <cleysonph@gmail.com>
2022-05-29 21:27:07 -0300

    feat: create find all projects route

--- main.go
+++ main.go
@@ -5,11 +5,21 @@ import (
 	"net/http"
 	"time"
 
+	"github.com/cleysonph/project-manager/api/controller"
+	"github.com/cleysonph/project-manager/api/router"
+	"github.com/cleysonph/project-manager/repository"
+	"github.com/cleysonph/project-manager/usecase"
 	"github.com/gorilla/mux"
 )
 
 func main() {
 	r := mux.NewRouter()
+
+	projectRepo := repository.NewInMemoryProjectRepository()
+	projectService := usecase.NewProjectService(projectRepo)
+	projectController := controller.NewProjectController(projectService)
+
+	router.InitProjectRoutes(r, projectController)
 
 	server := &http.Server{
 		Handler:      r,
